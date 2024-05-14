814b82af36c5fcd5ba2fabe9a461b172a42313c3
piyashah123 <82143407+piyashah123@users.noreply.github.com>
2024-05-14 21:57:33 +0530

    Add files via upload

---
+++ paintball-arena.js
@@ -0,0 +1,9 @@
+<!--A-Frame Environment-->
+<a-entity
+    id="environment" Environment="preset:default ; skyType:gradient ; lighting:point">
+</a-entity>
+<!--
+    A-Frame Environment preset values to choose from:
+
+    
+-->
